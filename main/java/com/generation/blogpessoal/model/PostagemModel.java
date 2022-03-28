8776518178b56c5962231e23e998dfe0a34223f1
Jenny Gritti <jenny.gritti@gmail.com>
2022-03-28 14:42:31 -0300

    Relacionamento

--- main/java/com/generation/blogpessoal/model/PostagemModel.java
+++ main/java/com/generation/blogpessoal/model/PostagemModel.java
@@ -6,11 +6,14 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
 import org.hibernate.annotations.UpdateTimestamp;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 @Entity // transforma em tabela
 @Table(name="tb_postagens") //nomeia a tabela
@@ -30,6 +33,10 @@ public class PostagemModel {
 	@UpdateTimestamp //usa o mesmo horário do computador
 	private LocalDateTime data;
 
+	@ManyToOne
+	@JsonIgnoreProperties("postagem")
+	private Tema tema;
+	
 	public Long getId() {
 		return id;
 	}
@@ -60,6 +67,14 @@ public class PostagemModel {
 
 	public void setData(LocalDateTime data) {
 		this.data = data;
+	}
+
+	public Tema getTema() {
+		return tema;
+	}
+
+	public void setTema(Tema tema) {
+		this.tema = tema;
 	}
 
 	
